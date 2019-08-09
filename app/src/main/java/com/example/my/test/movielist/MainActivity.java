dc426372440169f5bce64e2f17a666b009e17e80
Takhir Nafikov <takhir.learn@gmail.com>
2019-08-09 10:09:21 +0300

    Added Picasso

--- app/src/main/java/com/example/my/test/movielist/MainActivity.java
+++ app/src/main/java/com/example/my/test/movielist/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.my.test.movielist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.example.my.test.movielist.data.Movie;
@@ -14,16 +16,19 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerViewPosters;
+    private MovieAdapter movieAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 5);
+        recyclerViewPosters = findViewById(R.id.recyclerViewPosters);
+        recyclerViewPosters.setLayoutManager(new GridLayoutManager(this, 2));
+        movieAdapter = new MovieAdapter();
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 1);
         ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
-        StringBuilder builder = new StringBuilder();
-        for (Movie movie : movies) {
-            builder.append(movie.getTitle()).append("\n");
-        }
-        Log.i("MyResult", builder.toString());
+        movieAdapter.setMovies(movies);
+        recyclerViewPosters.setAdapter(movieAdapter);
     }
 }
